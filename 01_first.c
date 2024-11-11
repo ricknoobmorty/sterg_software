225f8348311653e804e00707c83f4213294cec99
ricknoobmorty <sunnykumar.p76@gmail.com>
2024-11-11 17:48:53 +0530

    Added a new line to c file

--- 01_first.c
+++ 01_first.c
@@ -3,7 +3,9 @@
 int main(){
     printf("Hello World\n How are you?");
     printf("Hi\n");
-    printf("Learning to use pull in Git");
+    printf("Learning to use pull in Git\n");
+    printf("Learning to use push in Git\n");
+    
     
     return 0;
 }
