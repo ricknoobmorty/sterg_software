54c78a2a5f7a9edb5fc696c941c8ba9232916703
Sunny Pandey <sunnykumar.p76@gmail.com>
2024-11-11 17:53:48 +0530

    Update 01_first.c
    added one more line to the c file

--- 01_first.c
+++ 01_first.c
@@ -5,6 +5,7 @@ int main(){
     printf("Hi\n");
     printf("Learning to use pull in Git\n");
     printf("Learning to use push in Git\n");
+    printf("Made a new branch using GITHUB");
     
     
     return 0;
